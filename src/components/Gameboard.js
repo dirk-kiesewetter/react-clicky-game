ce41c8e30661cdc937ab55031f50d6ba4a464b1f
Dirk Kiesewetter <dirk.k1@outlook.com>
2019-02-11 22:09:05 -0500

    initial layout - images added & CSS changes

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,23 +1,121 @@
 import React from "react";
+import Awing from "../assets/images/a-wing.png";
+import Bwing from "../assets/images/b-wing.jpg";
+import Corvette from "../assets/images/corellian_corvette.jpg";
+import Falcon from "../assets/images/millennium-falcon.jpg";
+import Shuttle from "../assets/images/shuttle.jpg";
+import DeathStar from "../assets/images/death-star.jpg";
+import SSD from "../assets/images/ssd.jpg";
+import Ywing from "../assets/images/y-wing.jpg";
+import TIE from "../assets/images/tie-fighter.jpg";
+import TieInt from "../assets/images/tie-interceptor.jpg";
+import StarDestroyer from "../assets/images/stardestroyer.jpg";
+import Xwing from "../assets/images/xwing.jpg";
 
 class Gameboard extends React.Component {
   render() {
     return (
-      <div>
+      <div id="gamePieces">
+        <br />
         <h1>Gameboard</h1>
         <input
           type="image"
-          src="../../public/images/a-wing.png"
+          className="imgBtn"
+          src={Awing}
           alt="Submit"
-          width="48"
-          height="48"
+          width="150"
+          height="150"
         />
         <input
           type="image"
-          src="~/Images/Desert.jpg"
+          className="imgBtn"
+          src={Bwing}
           alt="Submit"
-          width="48"
-          height="48"
+          width="150"
+          height="150"
+        />
+
+        <input
+          type="image"
+          className="imgBtn"
+          src={Falcon}
+          alt="Submit"
+          width="150"
+          height="150"
+        />
+        <input
+          type="image"
+          className="imgBtn"
+          src={Shuttle}
+          alt="Submit"
+          width="150"
+          height="150"
+        />
+        <br />
+        <input
+          type="image"
+          className="imgBtn"
+          src={DeathStar}
+          alt="Submit"
+          width="150"
+          height="150"
+        />
+        <input
+          type="image"
+          className="imgBtn"
+          src={SSD}
+          alt="Submit"
+          width="150"
+          height="150"
+        />
+        <input
+          type="image"
+          className="imgBtn"
+          src={Ywing}
+          alt="Submit"
+          width="150"
+          height="150"
+        />
+        <input
+          type="image"
+          className="imgBtn"
+          src={TIE}
+          alt="Submit"
+          width="150"
+          height="150"
+        />
+        <br />
+        <input
+          type="image"
+          className="imgBtn"
+          src={TieInt}
+          alt="Submit"
+          width="150"
+          height="150"
+        />
+        <input
+          type="image"
+          className="imgBtn"
+          src={StarDestroyer}
+          alt="Submit"
+          width="150"
+          height="150"
+        />
+        <input
+          type="image"
+          className="imgBtn"
+          src={Xwing}
+          alt="Submit"
+          width="150"
+          height="150"
+        />
+        <input
+          type="image"
+          className="imgBtn"
+          src={Corvette}
+          alt="Submit"
+          width="150"
+          height="150"
         />
       </div>
     );
