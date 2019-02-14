cf7d9425a813e3df24fa5c7f83b967a0974c4866
Dirk Kiesewetter <dirk.k1@outlook.com>
2019-02-13 20:45:39 -0500

    added logic for start/correct guess/incorrect guess span

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -15,14 +15,28 @@ class Gameboard extends React.Component {
     // console.log(id)
 
     for (let i = 0; i < starWarsCopy.length; i++) {
-      if (starWarsCopy[i].id == id) {
+      // user loss condition
+      if (starWarsCopy[i].id === id) {
         if (starWarsCopy[i].clicked) {
+          document.getElementById("correctGuessAlert").style.display = "none";
+          document.getElementById("gameStartAlert").style.display = "none";
+          document.getElementById("incorrectGuessAlert").style.display = "block";
+
           console.log("game over, man!")
+
+          // sets new high score
           if (this.gameScore > this.highScore) { this.gameScore = this.highScore }
           this.gameReset();
         }
         else {
+          // changes state of gamepiece to 'clicked'
           starWarsCopy[i].clicked = true;
+          document.getElementById("incorrectGuessAlert").style.display = "none";
+          document.getElementById("correctGuessAlert").style.display = "block";
+          document.getElementById("gameStartAlert").style.display = "none";
+
+
+
           let currentScore = this.state.gameScore;
           currentScore++
           let currentHighScore = this.state.highScore;
@@ -30,7 +44,7 @@ class Gameboard extends React.Component {
             currentHighScore = currentScore;
           }
           this.setState({ starWarsImages: this.shuffleCards(starWarsCopy), gameScore: currentScore, highScore: currentHighScore });
-          console.log("cs:", currentScore, "hs:", currentHighScore)
+          console.log("cs:", currentScore, "chs:", currentHighScore)
         }
       }
     }
@@ -52,18 +66,25 @@ class Gameboard extends React.Component {
 
   gameReset = () => {
     this.setState({ starWarsImages: this.shuffleCards(starWarsImages), gameScore: 0 });
+    setTimeout(() => {
+      document.getElementById("incorrectGuessAlert").style.display = "none";
+      document.getElementById("gameStartAlert").style.display = "block";
+    }, 500)
+
   }
 
   render = () => {
     return (
       <div>
         <br />
-        <div id="scoreBox"><span>your score:{this.currentScore}  |  Game high score:{this.currenthighScore}</span></div>
+        <div id="scoreBox"><span>your score:{this.currentScore}  |  Game high score:{this.currentHighScore}</span></div>
 
         <br />
-        <h1 className="gameHeader">Click on any image to start</h1>
-        <h2>Click only on images that you haven't clicked yet.</h2>
+        <h1 className="gameHeader">Click on an image to start, <br />but don't click on an image more than once.</h1>
         <hr />
+        <h2 id="gameStartAlert">Click any image to begin</h2>
+        <h2 id="correctGuessAlert">You guessed correctly! </h2>
+        <h2 id="incorrectGuessAlert">You guessed incorrectly! </h2>
         <div className="container">
 
           {this.state.starWarsImages.map(starWarsImages => (
@@ -73,7 +94,6 @@ class Gameboard extends React.Component {
               name={starWarsImages.name}
               src={starWarsImages.link}
               clicked={this.gamePieceClicked}
-
             />
           ))}
         </div>
