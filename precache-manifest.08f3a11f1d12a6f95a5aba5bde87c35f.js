bc898b0456e01f62711b8b618ccdc68b3eeb34c2
Dirk Kiesewetter <dirk.k1@outlook.com>
2019-02-15 11:54:49 -0500

    Updates

---
+++ precache-manifest.08f3a11f1d12a6f95a5aba5bde87c35f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b580175debdf458a566037b3c08620c9",
+    "url": "/react-clicky-game/static/media/texture-wallpaper-56.b580175d.jpg"
+  },
+  {
+    "revision": "07c95f7184c1a76ef7a1",
+    "url": "/react-clicky-game/static/js/runtime~main.07c95f71.js"
+  },
+  {
+    "revision": "f860423d94f0840eb551",
+    "url": "/react-clicky-game/static/js/main.f860423d.chunk.js"
+  },
+  {
+    "revision": "ccd149fe0660bd90c366",
+    "url": "/react-clicky-game/static/js/2.ccd149fe.chunk.js"
+  },
+  {
+    "revision": "f860423d94f0840eb551",
+    "url": "/react-clicky-game/static/css/main.83ba018b.chunk.css"
+  },
+  {
+    "revision": "6f0fffe87c340f88dc4ef02950659010",
+    "url": "/react-clicky-game/index.html"
+  }
+];
